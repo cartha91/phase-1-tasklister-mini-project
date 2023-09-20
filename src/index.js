b180cfa05e040429ca2c032ebb2afff3e9fa0ac7
steven dulworth <crowscoupetech@proton.me>
2023-09-20 18:09:47 -0500

    add, delete, edit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const todoForm = document.getElementById('create-task-form');
+  const todoList = document.getElementById('tasks');
+
+  todoForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    addNewTodo(event.target['new-task-description'].value);
+    event.target.reset();
+  });
+
+  function addNewTodo(task) {
+    const li = document.createElement('li');
+    const taskText = document.createTextNode(task);
+    li.appendChild(taskText);
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+    checkbox.addEventListener('change', function() {
+      if (checkbox.checked && confirm('You really wanna delete it?')) {
+        li.remove();
+      }
+    });
+
+    const editTodo = document.createElement('button');
+    editTodo.textContent = 'edit';
+    editTodo.addEventListener('click', function() {
+      const changedTodo = prompt('Change it up a bit!:', taskText.textContent);
+      if (changedTodo !== null) {
+        taskText.textContent = changedTodo;
+      }
+    });
+
+    li.appendChild(checkbox)
+    li.appendChild(editTodo)
+    todoList.appendChild(li);
+  }
 });
